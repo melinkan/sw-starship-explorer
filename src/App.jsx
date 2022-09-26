213a4ad3f7e47fe96cfa6f1a01d371e220520942
Melin Kan <melin.kan@outlook.com>
2022-09-26 15:42:57 +0300

    Implement basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header, Particles } from "./components";
+import { StarshipsProvider } from "./contexts/StarshipsContext";
+import { Home } from "./routes/Home";
+import { Detail } from "./routes/Detail";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <StarshipsProvider>
+        <Particles />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="characters/:id" element={<Detail />} />
+        </Routes>
+      </StarshipsProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
