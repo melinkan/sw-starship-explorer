3018a9180b9b1b7e49b7892ce6d9a91eeecf6b61
Melin Kan <melin.kan@outlook.com>
2022-09-26 15:31:27 +0300

    Add starship assets

---
+++ src/imageRoutes.js
@@ -0,0 +1,183 @@
+import cr90Corvette from "./assets/starships/cr90-corvette.png";
+import starDestroyer from "./assets/starships/star-destroyer.png";
+import sentinelClassLandingCraft from "./assets/starships/sentinel-class.png";
+import deathStar from "./assets/starships/death-star.png";
+import millenniumFalcon from "./assets/starships/millenium-falcon.png";
+import yWing from "./assets/starships/y-wing.png";
+import xWing from "./assets/starships/x-wing.png";
+import tieAdvancedx1 from "./assets/starships/tie-advanced.png";
+import executor from "./assets/starships/executor.png";
+import rebelTransport from "./assets/starships/rebel-transport.png";
+import slave1 from "./assets/starships/slave-1.png";
+import imperialShuttle from "./assets/starships/imperial-shuttle.png";
+import ef76NebulonBEscortFrigate from "./assets/starships/EF76Nebulon-B-escort-frigate.png";
+import calamariCruiser from "./assets/starships/Calamari-Cruiser.png";
+import aWing from "./assets/starships/A-wing.png";
+import bWing from "./assets/starships/B-wing.png";
+import republicCruiser from "./assets/starships/Republic-Cruiser.png";
+import droidControlShip from "./assets/starships/Droid-control-ship.png";
+import nabooFighter from "./assets/starships/Naboo-fighter.png";
+import nabooRoyalStarship from "./assets/starships/Naboo-Royal-Starship.png";
+import nabooStarSkiff from "./assets/starships/Naboo-star-skiff.png";
+import jediInterceptor from "./assets/starships/Jedi-Interceptor.png";
+import arc170 from "./assets/starships/arc-170.png";
+import bankingClanFrigte from "./assets/starships/Banking-clan-frigte.png";
+import belbullab22Starfighter from "./assets/starships/Belbullab-22-starfighter.png";
+import vWing from "./assets/starships/V-wing.png";
+import scimitar from "./assets/starships/Scimitar.png";
+import jTypeDiplomaticBarge from "./assets/starships/J-type-diplomatic-barge.png";
+import aa9CoruscantFreighter from "./assets/starships/AA-9-Coruscant-freighter.png";
+import jediStarfighter from "./assets/starships/Jedi-starfighter.png";
+import hTypeNubianYacht from "./assets/starships/H-type-Nubian-yacht.png";
+import republicAssaultShip from "./assets/starships/Republic-Assault-ship.png";
+import solarSailer from "./assets/starships/solarsailer.png";
+import tradeFederationCruiser from "./assets/starships/Trade-Federation-cruiser.png";
+import thetaClassT2cShuttle from "./assets/starships/Theta-class-T-2c-shuttle.png";
+import republicAttackCruiser from "./assets/starships/Republic-attack-cruiser.png";
+
+export const starshipAssets = [
+  {
+    name: "CR90 corvette",
+    path: cr90Corvette,
+  },
+  {
+    name: "Star Destroyer",
+    path: starDestroyer,
+  },
+  {
+    name: "Sentinel-class landing craft",
+    path: sentinelClassLandingCraft,
+  },
+  {
+    name: "Death Star",
+    path: deathStar,
+  },
+  {
+    name: "Millennium Falcon",
+    path: millenniumFalcon,
+  },
+  {
+    name: "Y-wing",
+    path: yWing,
+  },
+  {
+    name: "X-wing",
+    path: xWing,
+  },
+  {
+    name: "TIE Advanced x1",
+    path: tieAdvancedx1,
+  },
+  {
+    name: "Executor",
+    path: executor,
+  },
+  {
+    name: "Rebel transport",
+    path: rebelTransport,
+  },
+  {
+    name: "Slave 1",
+    path: slave1,
+  },
+  {
+    name: "Imperial shuttle",
+    path: imperialShuttle,
+  },
+  {
+    name: "EF76 Nebulon-B escort frigate",
+    path: ef76NebulonBEscortFrigate,
+  },
+  {
+    name: "Calamari Cruiser",
+    path: calamariCruiser,
+  },
+  {
+    name: "A-wing",
+    path: aWing,
+  },
+  {
+    name: "B-wing",
+    path: bWing,
+  },
+  {
+    name: "Republic Cruiser",
+    path: republicCruiser,
+  },
+  {
+    name: "Droid control ship",
+    path: droidControlShip,
+  },
+  {
+    name: "Naboo fighter",
+    path: nabooFighter,
+  },
+  {
+    name: "Naboo Royal Starship",
+    path: nabooRoyalStarship,
+  },
+  {
+    name: "Naboo star skiff",
+    path: nabooStarSkiff,
+  },
+  {
+    name: "Jedi Interceptor",
+    path: jediInterceptor,
+  },
+  {
+    name: "arc-170",
+    path: arc170,
+  },
+  {
+    name: "Banking clan frigte",
+    path: bankingClanFrigte,
+  },
+  {
+    name: "Belbullab-22 starfighter",
+    path: belbullab22Starfighter,
+  },
+  {
+    name: "V-wing",
+    path: vWing,
+  },
+  {
+    name: "Scimitar",
+    path: scimitar,
+  },
+  {
+    name: "J-type diplomatic barge",
+    path: jTypeDiplomaticBarge,
+  },
+  {
+    name: "AA-9 Coruscant freighter",
+    path: aa9CoruscantFreighter,
+  },
+  {
+    name: "Jedi starfighter",
+    path: jediStarfighter,
+  },
+  {
+    name: "H-type Nubian yacht",
+    path: hTypeNubianYacht,
+  },
+  {
+    name: "Republic Assault ship",
+    path: republicAssaultShip,
+  },
+  {
+    name: "Solar Sailer",
+    path: solarSailer,
+  },
+  {
+    name: "Trade Federation cruiser",
+    path: tradeFederationCruiser,
+  },
+  {
+    name: "Theta-class T-2c shuttle",
+    path: thetaClassT2cShuttle,
+  },
+  {
+    name: "Republic attack cruiser",
+    path: republicAttackCruiser,
+  },
+];
